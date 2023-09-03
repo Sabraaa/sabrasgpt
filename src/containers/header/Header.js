070d4fabf6dda08a70bad49d7fc63e3679d0e84a
Sabra <100771060+Sabraaa@users.noreply.github.com>
2023-09-03 12:09:59 +0330

    wrapper

--- src/containers/header/Header.js
+++ src/containers/header/Header.js
@@ -5,7 +5,7 @@ import ai from "../../assets/ai.png";
 
 const Header = () => {
   return (
-    <div className="gpt__header section__padding" id="home">
+    <div className="gpt__header-wrapper section__padding" id="home">
       <div className="gpt__header-content">
         <h1 className="gradient__text">
           Let's Build Something amazing with GPT-3 Open AI
